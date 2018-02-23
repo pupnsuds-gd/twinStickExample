fbc150f7066eccea0e1ccd774bc8d8be06e0a620
Benny <bennybelanger@gmail.com>
2018-02-23 12:30:37 -0500

    added bullet time to live

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -4,6 +4,7 @@ extends Area2D
 # var a = 2
 # var b = "textvar"
 var speed = 150
+var timeToLive = 20.0
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -12,5 +13,9 @@ func _ready():
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
+	if timeToLive < 0:
+		queue_free()
+	else:
+		timeToLive -= 1 * delta
 	move_local_x(speed * delta)
 	pass
